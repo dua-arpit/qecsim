8b7df124efd7da9e22ab34f2f639ea165040abc8
Arpit Dua <arpit.dua@yale.edu>
2021-04-02 01:09:14 -0400

    Update defTND.py

--- demos/defTND.py
+++ demos/defTND.py
@@ -17,7 +17,6 @@ import os,time
 import multiprocessing as mp
 from functools import partial
 
-
 def parallel_step_p(code,error_model,decoder,max_runs,perm_rates,code_name,error_probability):
     # perm_mat,perm_vec= deform_matsvecs(code,decoder,error_model)
     result= app_def.run_def(code,error_model,decoder,error_probability,perm_rates,code_name,max_runs)
@@ -35,6 +34,52 @@ def parallel_step_code(code,error_model,decoder,max_runs,perm_rates,code_name,er
 
     return [pL_list,std_list]
 
+def TNDresult(code,decoder,error_model,max_runs,perm_rates,error_probabilities,code_name,num_realiz):
+    pL_list_realiz=np.zeros((num_realiz,len(error_probabilities)))
+    std_list_realiz=np.zeros((num_realiz,len(error_probabilities)))      
+    
+    pL_list=np.zeros(len(error_probabilities))
+    std_list=np.zeros(len(error_probabilities))
+    log_pL_list=np.zeros(len(error_probabilities))
+    log_std_list=np.zeros(len(error_probabilities))
+    
+    if code_name=='random_all' or code_name=='random_XY' or code_name=='random_XZ':
+        p=mp.Pool()
+        func=partial(parallel_step_code,code,error_model,decoder,max_runs,perm_rates,code_name,error_probabilities)
+        result=p.map(func,range(num_realiz))
+        #print(result)
+        p.close()
+        p.join()
+        for realization_index in range(num_realiz):
+            for i in range(len(error_probabilities)):
+                pL_list_realiz[realization_index][i]=result[realization_index][0][i]
+                std_list_realiz[realization_index][i]=result[realization_index][1][i]
+        
+        pL_list = np.sum(pL_list_realiz,axis=0)/num_realiz
+        std_list = np.sqrt(np.sum(vsquare(std_list_realiz),axis=0))/num_realiz
+
+        for i in range(len(pL_list)):
+            log_pL_list[i]=-np.log(pL_list[i])
+            log_std_list[i]=std_list[i]/(pL_list[i]*np.log(10))
+
+    else:
+        p=mp.Pool()
+        func=partial(parallel_step_p,code,error_model,decoder,max_runs,perm_rates,code_name)
+        result=p.map(func,error_probabilities)
+        print(result)
+        p.close()
+        p.join()
+        
+        for i in range(len(result)):
+            pL_list[i]=result[i][0]   
+            std_list[i]=result[i][1]
+            log_pL_list[i]=-np.log(pL_list[i])
+            log_std_list[i]=std_list[i]/(pL_list[i]*np.log(10))
+
+    return [pL_list,std_list,log_pL_list,log_std_list]
+
+
+
 def square(a):
     return a**2
 
@@ -43,135 +88,109 @@ vsquare=np.vectorize(square)
 # set models
 sizes= range(6,11,2)
 codes_and_size = [PlanarCode(*(size,size)) for size in sizes]
-bias_list=[10]
+bias_list=[0.5,10,40,100,300,1000,10**300]
 
 layout_name='planar'
-
-code_name='spiral'
-code_name='XZZX'
-code_name='random'
-
-code_name='random'
-code_name='random_Hadamard'
-code_name='CSS'
-
-layout_name='planar'
-
-if (code_name=='random' or code_name=='random_Hadamard'):
-    num_realiz=40
-else:
-    num_realiz=1
+code_names=['XZZX','XY','CSS','spiral_XZ','random_XZ','random_all','random_XY']
 
 # set physical error probabilities
-error_probability_min,error_probability_max = 0.07,0.4
+error_probability_min,error_probability_max = 0.07,0.5
 error_probabilities = np.linspace(error_probability_min,error_probability_max,20)
 
 timestr = time.strftime('%Y%m%d-%H%M%S ')   #record current date and time
-dirname='./data/'+timestr+layout_name+code_name
+dirname='./data/'+timestr+layout_name+'all_codes'
 os.mkdir(dirname)     
 
 for bias in bias_list:
-    error_model = BiasedDepolarizingErrorModel(bias,'Y')
     # bias=1/bias
     # error_model=BiasedYXErrorModel(bias)
-    chi_val=10
+    chi_val=12
     decoder = _planarmpsdecoder_def.PlanarMPSDecoder_def(chi=chi_val)
 
-    # set max_runs for each probability
-    max_runs = 20000
+    for code_name in code_names:
+        if code_name=='CSS':
+            num_realiz=1
+            bias_str='Z'
+            max_runs=20000
+        elif code_name=='XY':
+            bias_str='Y'
+            num_realiz=1
+            max_runs=20000
+        elif code_name=='XZZX':
+            num_realiz=1
+            bias_str='Z'
+            max_runs=20000
+        elif code_name=='spiral_XZ':
+            num_realiz=1
+            bias_str='Z'
+            max_runs=20000
+        elif code_name=='random_all':
+            num_realiz=40
+            bias_str='Z'
+            max_runs=2000
+            perm_rates=[1/3,1/3,1/3,0,0,0]
+        elif code_name=='random_XZ':
+            num_realiz=40
+            bias_str='Z'
+            max_runs=2000
+            perm_rates=[1/2,1/2,0,0,0,0]
+        elif code_name=='random_XY':
+            num_realiz=40
+            bias_str='Y'
+            max_runs=2000
+            perm_rates=[1/2,1/2,0,0,0,0]
 
-    # print run parameters
-    print('code_name:',code_name)
-    print('codes_and_size:',[code.label for code in codes_and_size])
-    print('Error model:',error_model.label)
-    print('Decoder:',decoder.label)
-    print('Error probabilities:',error_probabilities)
-    print('Maximum runs:',max_runs)
+        error_model = BiasedDepolarizingErrorModel(bias,bias_str)
 
-    pL_list =np.zeros((len(codes_and_size),len(error_probabilities)))
-    std_list=np.zeros((len(codes_and_size),len(error_probabilities)))
+        # print run parameters
+        print('code_name:',code_name)
+        print('codes_and_size:',[code.label for code in codes_and_size])
+        print('Error model:',error_model.label)
+        print('Decoder:',decoder.label)
+        print('Error probabilities:',error_probabilities)
+        print('Maximum runs:',max_runs)
 
-    log_pL_list =np.zeros((len(codes_and_size),len(error_probabilities)))
-    log_std_list=np.zeros((len(codes_and_size),len(error_probabilities)))
+        pL_list =np.zeros((len(codes_and_size),len(error_probabilities)))
+        std_list=np.zeros((len(codes_and_size),len(error_probabilities)))
 
-    pL_list_realiz=np.zeros((len(codes_and_size),num_realiz,len(error_probabilities)))
-    std_list_realiz=np.zeros((len(codes_and_size),num_realiz,len(error_probabilities)))  
+        log_pL_list =np.zeros((len(codes_and_size),len(error_probabilities)))
+        log_std_list=np.zeros((len(codes_and_size),len(error_probabilities)))
 
-    perm_rates=[0,0,0,0,0,0]
+        pL_list_realiz=np.zeros((len(codes_and_size),num_realiz,len(error_probabilities)))
+        std_list_realiz=np.zeros((len(codes_and_size),num_realiz,len(error_probabilities)))  
 
-    for L_index,code in enumerate(codes_and_size):
+        perm_rates=[0,0,0,0,0,0]
 
-        if code_name=='random' or code_name=='random_Hadamard':
-            #permutations for random code
-            if code_name=='random':
-                # perm_rates=[1/4,1/4,1/4,1/4,0,0]
-                perm_rates=[1/6,1/6,1/6,1/6,1/6,1/6]
-            else:
-                perm_rates=[1/2,1/2,0,0,0,0]
+        for L_index,code in enumerate(codes_and_size):
+            [pL_list[L_index],std_list[L_index],log_pL_list[L_index],log_std_list[L_index]]=TNDresult(code,decoder,error_model,max_runs,perm_rates,error_probabilities,code_name,num_realiz)
 
-            for realization_index in range(num_realiz):
-                # perm_mat,perm_vec= deform_matsvecs(code,decoder,error_model)
-                p=mp.Pool()
-                func=partial(parallel_step_p,code,error_model,decoder,max_runs,perm_rates,code_name)
-                result=p.map(func,error_probabilities)
-                #print(result)
-                p.close()
-                p.join()
-                for i in range(len(error_probabilities)):
-                    pL_list_realiz[L_index][realization_index][i]=result[i][0]
-                    std_list_realiz[L_index][realization_index][i]=result[i][1]
-                
-            pL_list[L_index] = np.sum(pL_list_realiz[L_index],axis=0)/num_realiz
-            std_list[L_index] = np.sqrt(np.sum(vsquare(std_list_realiz[L_index]),axis=0))/num_realiz
+        np.savetxt(dirname+"/p_list+"+code_name+".csv",error_probabilities,delimiter=",")
+        np.savetxt(dirname+"/pL_list+"+code_name+".csv",pL_list,delimiter=",")
+        np.savetxt(dirname+"/std_list.csv",std_list,delimiter=",")
 
-            for i in range(len(pL_list[L_index])):
-                log_pL_list[L_index][i]=-np.log(pL_list[L_index][i])
-                log_std_list[L_index][i]=std_list[L_index][i]/(pL_list[L_index][i]*np.log(10))
+        from itertools import cycle
+        plt.figure(figsize=(20,10))
+        lines = ['-',':','--','-.']
+        linecycler = cycle(lines)
+        plt.title('TND at bias='+str(bias)[:7]+' and chi='+str(chi_val)+' for '+layout_name+' '+code_name)
+        for sizes_index,size in enumerate(sizes):
+            plt.errorbar(error_probabilities,pL_list[sizes_index],std_list[sizes_index])
+        plt.xlabel('p')
+        plt.ylabel('$p_L$')
+        plt.legend(sizes) 
+        plt.savefig(dirname+'/threshold_plot_bias_'+str(bias)[:7]+'.pdf')
 
-        else:
-            # perm_mat,perm_vec= deform_matsvecs(code,decoder,error_model)
-            p=mp.Pool()
-            func=partial(parallel_step_p,code,error_model,decoder,max_runs,perm_rates,code_name)
-            result=p.map(func,error_probabilities)
-            print(result)
-            p.close()
-            p.join()
-            
-            for i in range(len(result)):
-                pL_list[L_index][i]=result[i][0]   
-                std_list[L_index][i]=result[i][1]
-                log_pL_list[L_index][i]=-np.log(pL_list[L_index][i])
-                log_std_list[L_index][i]=std_list[L_index][i]/(pL_list[L_index][i]*np.log(10))
-
-
-
-    np.savetxt(dirname+"/p_list.csv",error_probabilities,delimiter=",")
-    np.savetxt(dirname+"/pL_list.csv",pL_list,delimiter=",")
-    np.savetxt(dirname+"/std_list.csv",std_list,delimiter=",")
-
-    from itertools import cycle
-    plt.figure(figsize=(20,10))
-    lines = ['-',':','--','-.']
-    linecycler = cycle(lines)
-    plt.title('TND at bias='+str(bias)[:7]+' and chi='+str(chi_val)+' for '+layout_name+' '+code_name)
-    for sizes_index,size in enumerate(sizes):
-        plt.errorbar(error_probabilities,pL_list[sizes_index],std_list[sizes_index])
-    plt.xlabel('p')
-    plt.ylabel('$p_L$')
-    plt.legend(sizes) 
-    plt.savefig(dirname+'/threshold_plot_bias_'+str(bias)[:7]+'.pdf')
-
-    from itertools import cycle
-    plt.figure(figsize=(20,10))
-    lines = ['-',':','--','-.']
-    linecycler = cycle(lines)
-    plt.title('TND failure rate scaling at bias='+str(bias)[:7]+' and chi='+str(chi_val)+' for '+layout_name+' '+code_name)
-    for sizes_index,size in enumerate(sizes):
-        plt.errorbar(error_probabilities,log_pL_list[sizes_index],log_std_list[sizes_index])
-    plt.xlabel('p')
-    plt.ylabel('$-log(p_L)$')
-    plt.legend(sizes) 
-    plt.savefig(dirname+'/log_scaling_plot_bias_'+str(bias)[:7]+'.pdf')
+        from itertools import cycle
+        plt.figure(figsize=(20,10))
+        lines = ['-',':','--','-.']
+        linecycler = cycle(lines)
+        plt.title('TND failure rate scaling at bias='+str(bias)[:7]+' and chi='+str(chi_val)+' for '+layout_name+' '+code_name)
+        for sizes_index,size in enumerate(sizes):
+            plt.errorbar(error_probabilities,log_pL_list[sizes_index],log_std_list[sizes_index])
+        plt.xlabel('p')
+        plt.ylabel('$-log(p_L)$')
+        plt.legend(sizes) 
+        plt.savefig(dirname+'/log_scaling_plot_bias_'+str(bias)[:7]+'.pdf')
 
 
 
